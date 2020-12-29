77fe3f91c6a844f2332af98cf80aebcede68fc70
RajatVermaz <49203000+RajatVermaz@users.noreply.github.com>
2020-12-29 11:34:21 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+# Madlibs game based on string concatination
+
+adj1 = input('Adjective : ')
+adj2 = input('Adjective : ')
+verb1 = input('Verb : ')
+verb2 = input('Verb : ')
+
+
+print(f'Computer programming is so {adj1} it makes me {verb1}, and some day it will be {adj2} and \
+computer will {verb2}.')
+
